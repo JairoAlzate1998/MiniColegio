62719ccd28014b639118e2287dc34a5f418533bf
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-26 10:48:46 -0500

    Add form student

--- frontend/src/app/home/register-student/register-student.component.ts
+++ frontend/src/app/home/register-student/register-student.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register-student',
   templateUrl: './register-student.component.html',
-  styleUrls: ['./register-student.component.css']
+  styleUrls: ['./register-student.component.css'],
 })
 export class RegisterStudentComponent implements OnInit {
+  registerData: any;
+  message: string;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.registerData = {};
+    this.message = '';
   }
 
+  ngOnInit(): void {}
+
+  registerUser() {}
 }
